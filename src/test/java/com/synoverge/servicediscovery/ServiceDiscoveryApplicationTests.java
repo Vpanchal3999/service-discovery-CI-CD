ee1941762e1c14facbe04e7f3fed058a68807b1b
Vishesh Panchal <panchavi@SYNOVERGETECH.COM>
2024-01-03 15:42:20 +0530

    first commit

---
+++ src/test/java/com/synoverge/servicediscovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.synoverge.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
